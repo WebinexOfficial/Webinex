4c2c4f635d3d2366a88f16c7c9794f23c7bc95c9
mukeshy2005 <my653197@gmail.com>
2025-04-18 13:31:17 +0530

    no changed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ const App = () => {
     <div>
       <Navbar/>
       <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+      <h1 className='text-blue-500'>search</h1>
       <p className='text-red-500'>This is my first react app</p>
       <p className='text-blue-500'>This is my first react app</p>
       <p className='text-green-500'>This is my first react app</p>
