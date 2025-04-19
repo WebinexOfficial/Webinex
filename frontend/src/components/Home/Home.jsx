f337c5db104a50e1b9f1f4e5d37da36c0cc729c8
mukeshy2005 <my653197@gmail.com>
2025-04-19 10:31:09 +0530

    portfolio section in home is added

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,11 +1,15 @@
 import Navbar from '../Header/Navbar';
 import PortfolioSection from '../Portfolio/Portfolio';
+import OurServices from '../OurServices/OurServices';
 
 const Home = () => {
   return (
     <>
       <Navbar />
+      <OurServices/>
       <PortfolioSection />
+      
+
       
     </>
   );
