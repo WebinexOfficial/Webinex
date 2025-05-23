60f3390d8f67e0f9b0644809f05e709d9f2a6db3
mukesh yadav <my653197@gmail.com>
2025-05-24 00:55:03 +0530

    delete some of the video

--- frontend/src/components/SignUp/SignUp.jsx
+++ frontend/src/components/SignUp/SignUp.jsx
@@ -29,7 +29,7 @@ const SignupPage = () => {
     }
 
     try {
-  const res = await fetch("http://localhost:5000/api/auth/register", {
+  const res = await fetch("https://webcraftbackend.onrender.com/api/auth/register", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
